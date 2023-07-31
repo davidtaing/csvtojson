966f98ed532f8e0c90d4e3f76e8a745d59042ae6
David Taing <adavidtaing@gmail.com>
2023-07-31 21:05:00 +1000

    refactor: marshalling logic to separate function

--- main.go
+++ main.go
@@ -12,29 +12,13 @@ func main() {
 
 	records, err := readCSV(p)
 	if err != nil {
-		fmt.Printf("Error reading csv %s\n", p)
+		fmt.Printf("Failed to read csv %s\n", p)
 		return
 	}
 
-	var data []map[string]string
-
-	if len(records) > 0 {
-		columnNames := records[0]
-
-		for _, row := range records[1:] {
-			record := make(map[string]string)
-			for i, value := range row {
-				if i < len(columnNames) {
-					record[columnNames[i]] = value
-				}
-			}
-			data = append(data, record)
-		}
-	}
-
-	jsonBytes, err := json.Marshal(data)
+	jsonBytes, err := marshalToJSON(records)
 	if err != nil {
-		fmt.Println("Error marshaling data:", err)
+		fmt.Printf("Failed to marshal csv data")
 		return
 	}
 
@@ -66,4 +50,30 @@ func readCSV(path string) ([][]string, error) {
 	}
 
 	return records, nil
+}
+
+func marshalToJSON(records [][]string) ([]byte, error) {
+	var data []map[string]string
+
+	if len(records) > 0 {
+		columnNames := records[0]
+
+		for _, row := range records[1:] {
+			record := make(map[string]string)
+			for i, value := range row {
+				if i < len(columnNames) {
+					record[columnNames[i]] = value
+				}
+			}
+			data = append(data, record)
+		}
+	}
+
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error marshaling data:", err)
+		return nil, err
+	}
+
+	return jsonBytes, nil
 }
