8ada029ab803a4eb30f1a2fb28fe3422edbff081
David Taing <adavidtaing@gmail.com>
2023-07-31 19:57:04 +1000

    write hello world to io.Pipe

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r, w := io.Pipe()
+
+	go func() {
+		fmt.Fprint(w, "Hello, World!\n")
+		w.Close()
+	}()
+
+	if _, err := io.Copy(os.Stdout, r); err != nil {
+		log.Fatal(err)
+	}
 }
